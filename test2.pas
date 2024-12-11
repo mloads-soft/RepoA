6d6a8b5fb1b8f9ea4ad6a1b76dd6fcbd1af4534d
mloads <86210243+mloads-soft@users.noreply.github.com>
2024-12-11 21:37:17 +0300

    Add files via upload

---
+++ test2.pas
@@ -0,0 +1,21 @@
+﻿program PR3;
+var
+  a: array[1..12] of integer;
+  i, sum: integer;
+begin
+  sum := 0;
+  // Ввод чисел
+  for i := 1 to 12 do
+  begin
+    Write('Введите число a', i, ': ');
+    ReadLn(a[i]);
+  end;
+  // Определение суммы четных чисел
+  for i := 1 to 12 do
+  begin
+    if a[i] mod 2 = 0 then
+      sum := sum + a[i];
+  end;
+
+  WriteLn('Сумма четных чисел: ', sum);
+end.
